10ef11a877687be0849fc243bab38327ff75ceba
Ederson Rodrigues <ederson.rodrigues777@gmail.com>
2025-02-13 12:58:37 -0300

    feat(main): Create infrastructure abstraction with db context and repositories

--- Core/CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
+++ Core/CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
@@ -8,6 +8,6 @@ public interface IBaseRepository<T> where T : BaseEntity
     void Update(T entity);
     void Delete(T entity);
     Task<T> Get(Guid id, CancellationToken cancellationToken);
-    Task<T> GetAll(CancellationToken cancellationToken);
+    Task<List<T>> GetAll(CancellationToken cancellationToken);
 }
 
